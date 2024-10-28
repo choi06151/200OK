8b9239d76326d8a7518b14a451b39c7f3a215e8b
kimdonghyuk <kdh367036@naver.com>
2024-10-28 15:35:20 +0900

    initgit add .

--- src/Routes/Contract.js
+++ src/Routes/Contract.js
@@ -5,49 +5,49 @@ import { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
 function Contract() {
-	let [gender, setGender] = useState('남');
-	const navigate = useNavigate();
+  let [gender, setGender] = useState('남');
+  const navigate = useNavigate();
 
-	function changeGender() {
-		if (gender == '남') {
-			setGender('여');
-		} else {
-			setGender('남');
-		}
-	}
+  function changeGender() {
+    if (gender == '남') {
+      setGender('여');
+    } else {
+      setGender('남');
+    }
+  }
 
-	return (
-		<div className={homestyle.div}>
-			<h2>계약 화면임</h2>
-			<div
-				style={{
-					width: '60%',
-					margin: '0 auto',
-					height: '40vh',
-					border: '1px solid red',
-					color: 'white',
-				}}
-			>
-				<h2>로고임</h2>
-			</div>
-			<div className="mt-3 mb-3" style={{ color: 'white' }}>
-				Gender :{' '}
-				<Button variant="light" onClick={changeGender}>
-					{gender}
-				</Button>
-			</div>
+  return (
+    <div className={homestyle.div}>
+      <h2>계약 화면입니다.</h2>
+      <div
+        style={{
+          width: '60%',
+          margin: '0 auto',
+          height: '40vh',
+          border: '1px solid red',
+          color: 'white',
+        }}
+      >
+        <h2>로고입니다.</h2>
+      </div>
+      <div className="mt-3 mb-3" style={{ color: 'white' }}>
+        Gender :{' '}
+        <Button variant="light" onClick={changeGender}>
+          {gender}
+        </Button>
+      </div>
 
-			<div>
-				<Button
-					onClick={() => {
-						navigate('/dodge');
-					}}
-				>
-					To Dodge
-				</Button>
-			</div>
-		</div>
-	);
+      <div>
+        <Button
+          onClick={() => {
+            navigate('/dodge');
+          }}
+        >
+          To Dodge
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Contract;
