5dc5ee72bd21dbeebdc5169515f101ed561f6604
Mieulchi <sangsangujk@hansung.ac.kr>
2024-11-06 22:24:59 +0900

    Job Until Middle Presentation

--- src/App.js
+++ src/App.js
@@ -8,38 +8,13 @@ import Ending from './Routes/Ending';
 import styles from './App.module.css';
 
 function App() {
-  // water, food, timeLeft 상태 관리
-  const [water, setWater] = useState(5);
-  const [food, setFood] = useState(5);
-  const [timeLeft, setTimeLeft] = useState(30);
-
   return (
     <div className={styles.App}>
       <div className={styles.shadow}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route
-            path="/dodge"
-            element={
-              <Dodge
-                water={water}
-                food={food}
-                timeLeft={timeLeft}
-                setWater={setWater}
-                setFood={setFood}
-                setTimeLeft={setTimeLeft}
-              />
-            }
-          />
-          <Route
-            path="/game"
-            element={
-              <Game
-                water={water}
-                food={food}
-              />
-            }
-          />
+          <Route path="/dodge" element={<Dodge />} />
+          <Route path="/game" element={<Game />} />
           <Route path="/wait" element={<Wait />} />
           <Route path="/end" element={<Ending />} />
         </Routes>
