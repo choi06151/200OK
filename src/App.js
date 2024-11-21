f9e4c3957721b3fbe8af5963872d1735231e8976
choi06151 <78068768+choi06151@users.noreply.github.com>
2024-11-21 12:52:12 +0900

    add panorama

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Routes/Home';
 import Dodge from './Routes/Dodge';
 import Game from './Routes/Game';
+import Panorama from './Routes/Panorama';
 import Wait from './Routes/Wait';
 import Ending from './Routes/Ending';
 import styles from './App.module.css';
+
+const imageList = [
+  'jungleexample.jpg',
+  'junglewoodback.avif',
+  'jungle.jpg',
+  'World.jpeg',  // Add as many images as you want
+];
 
 function App() {
   return (
@@ -17,6 +25,7 @@ function App() {
           <Route path="/game" element={<Game />} />
           <Route path="/wait" element={<Wait />} />
           <Route path="/end" element={<Ending />} />
+          <Route path="/panorama" element={<Panorama images={imageList} />} /> {/* Pass imageList as a prop */}
         </Routes>
       </div>
     </div>
