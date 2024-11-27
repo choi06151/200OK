cc58bb9cf70e845f896ed05303bed0c2226394e7
Mieulchi <sangsangujk@hansung.ac.kr>
2024-11-28 00:57:50 +0900

    회의내용 거의 완료

--- src/Components/Tooltip.js
+++ src/Components/Tooltip.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from '../Css/Tooltip.module.css';
 
-export default function Tooltip({ message, position, style }) {
-  return (
-    <div className={`${styles.tooltip} ${styles[position]}`} style={style}>
-      {message}
-    </div>
-  );
+export default function Tooltip({ message, position, style, visible }) {
+	return (
+		<div
+			className={`${styles.tooltip} ${visible ? styles.tooltipVisible : ''} ${
+				styles[position]
+			}`}
+			style={style}
+		>
+			{message}
+		</div>
+	);
 }
