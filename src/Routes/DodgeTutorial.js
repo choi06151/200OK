8f8b5122f704ada5af4fbe765d9d10bc5379a29e
choi06151 <78068768+choi06151@users.noreply.github.com>
2024-11-28 20:06:57 +0900

    fix button problem

--- src/Routes/DodgeTutorial.js
+++ src/Routes/DodgeTutorial.js
@@ -19,8 +19,12 @@ export default function DodgeTutorial() {
     setTimeout(() => {
       if (direction === 'left' && currentPage > 0) {
         setCurrentPage(currentPage - 1); // 이전 페이지
-      } else if (direction === 'right' && currentPage < images.length - 1) {
-        setCurrentPage(currentPage + 1); // 다음 페이지
+      } else if (direction === 'right') {
+        if (currentPage < images.length - 1) {
+          setCurrentPage(currentPage + 1); // 다음 페이지
+        } else {
+          navigate('/dodge'); // 마지막 페이지에서 오른쪽 화살표 클릭 시 이동
+        }
       }
       setFadeOut(false); // 이미지가 다시 나타나도록 설정
     }, 500); // fadeOut 효과 후 새로운 이미지로 전환
@@ -29,49 +33,34 @@ export default function DodgeTutorial() {
   const [fadeOut, setFadeOut] = useState(false);
 
   return (
-    <>
-      <div className={styles.tutorialContainer}>
-        <div className={styles.imageWrapper}>
-          <img
-            src={images[currentPage]}
-            alt={`Page ${currentPage + 1}`}
-            className={`${styles.tutorialImage} ${
-              fadeOut ? styles.fadeOut : styles.fadeIn
-            }`}
-          />
-        </div>
-        {/* 마지막 페이지 버튼 */}
-        {currentPage === images.length - 1 && (
-          <div className={styles.buttonWrapper}>
-            <img
-              src="/dog.png"
-              className={styles.gamestart}
-              onClick={() => {
-                navigate('/dodge');
-              }}
-            ></img>
-          </div>
-        )}
-        {/* 왼쪽 화살표 */}
-        {currentPage > 0 && (
-          <button
-            className={`${styles.arrowButton} ${styles.leftArrow}`}
-            onClick={() => handleArrowClick('left')}
-          >
-            &#8592;
-          </button>
-        )}
-
-        {/* 오른쪽 화살표 */}
-        {currentPage < images.length - 1 && (
-          <button
-            className={`${styles.arrowButton} ${styles.rightArrow}`}
-            onClick={() => handleArrowClick('right')}
-          >
-            &#8594;
-          </button>
-        )}
+    <div className={styles.tutorialContainer}>
+      <div className={styles.imageWrapper}>
+        <img
+          src={images[currentPage]}
+          alt={`Page ${currentPage + 1}`}
+          className={`${styles.tutorialImage} ${
+            fadeOut ? styles.fadeOut : styles.fadeIn
+          }`}
+        />
       </div>
-    </>
+
+      {/* 왼쪽 화살표 */}
+      {currentPage > 0 && (
+        <button
+          className={`${styles.arrowButton} ${styles.leftArrow}`}
+          onClick={() => handleArrowClick('left')}
+        >
+          &#8592;
+        </button>
+      )}
+
+      {/* 오른쪽 화살표 */}
+      <button
+        className={`${styles.arrowButton} ${styles.rightArrow}`}
+        onClick={() => handleArrowClick('right')}
+      >
+        &#8594;
+      </button>
+    </div>
   );
 }
--- src/Routes/MainGameTutorial.js
+++ src/Routes/MainGameTutorial.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styles from '../Css/MainGameTutorial.module.css';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-export default function MaingaimTutorial() {
+export default function MainGameTutorial() {
   const [currentPage, setCurrentPage] = useState(0);
   const images = [
     'MainTutorial1.png',
@@ -15,20 +15,26 @@ export default function MaingaimTutorial() {
 
   const navigate = useNavigate();
   const { state } = useLocation();
+  const [fadeOut, setFadeOut] = useState(false);
+
   const handleArrowClick = (direction) => {
     setFadeOut(true); // 이미지가 사라지도록 설정
 
     setTimeout(() => {
       if (direction === 'left' && currentPage > 0) {
         setCurrentPage(currentPage - 1); // 이전 페이지
-      } else if (direction === 'right' && currentPage < images.length - 1) {
-        setCurrentPage(currentPage + 1); // 다음 페이지
+      } else if (direction === 'right') {
+        if (currentPage < images.length - 1) {
+          setCurrentPage(currentPage + 1); // 다음 페이지
+        } else {
+          navigate('/game', {
+            state: { water: state.water, food: state.food },
+          }); // 마지막 페이지에서 클릭 시 이동
+        }
       }
       setFadeOut(false); // 이미지가 다시 나타나도록 설정
     }, 500); // fadeOut 효과 후 새로운 이미지로 전환
   };
-
-  const [fadeOut, setFadeOut] = useState(false);
 
   return (
     <div className={styles.tutorialContainer}>
@@ -53,28 +59,12 @@ export default function MaingaimTutorial() {
       )}
 
       {/* 오른쪽 화살표 */}
-      {currentPage < images.length - 1 && (
-        <button
-          className={`${styles.arrowButton} ${styles.rightArrow}`}
-          onClick={() => handleArrowClick('right')}
-        >
-          &#8594;
-        </button>
-      )}
-
-      {/* 마지막 페이지 버튼 */}
-      {currentPage === images.length - 1 && (
-        <button
-          className={styles.actionButton}
-          onClick={() => {
-            navigate('/game', {
-              state: { water: state.water, food: state.food },
-            });
-          }}
-        >
-          Game Start
-        </button>
-      )}
+      <button
+        className={`${styles.arrowButton} ${styles.rightArrow}`}
+        onClick={() => handleArrowClick('right')}
+      >
+        &#8594;
+      </button>
     </div>
   );
 }
